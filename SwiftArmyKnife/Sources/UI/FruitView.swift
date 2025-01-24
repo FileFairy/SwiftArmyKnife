acc1cc8911b47ee7803226573de1512c3d666b68
Matej Kokosinek <matej.dev@blauberg.tech>
2025-01-24 20:29:58 +0100

    add sub dependency

---
+++ SwiftArmyKnife/Sources/UI/FruitView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  SwiftArmyKnife
+//
+//  Created by Matej on 24. 1. 25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        VStack {
+            Text("🍊")
+            Text("UIQuit - sweet")
+        }
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
